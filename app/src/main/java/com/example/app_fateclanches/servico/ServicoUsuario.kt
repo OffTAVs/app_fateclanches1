90a91a39fb6a3c6b76651f68b6d86dc9826e9701
offtavs <otaviobarbosa2004@gmail.com>
2025-06-11 00:22:25 +0000

    POST

--- app/src/main/java/com/example/app_fateclanches/servico/ServicoUsuario.kt
+++ app/src/main/java/com/example/app_fateclanches/servico/ServicoUsuario.kt
@@ -1,30 +1,34 @@
 package com.example.app_fateclanches.servico
 
+import com.example.app_fateclanches.models.Usuario
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 
 interface ServicoUsuario {
-/*
-    @GET("contatos")
+
+
+
+    @POST("usuarios")
+    fun insereUsuarios(@Body usuario:Usuario): Call<Usuario>
+    /*
+     @GET("contatos")
     fun getContatos(): Call<List<Contato>>
+        @PUT("contatos/{id}")
+        fun editaContato(@Body contato:Contato, @Path("id") id:String): Call<Contato>
 
-    @POST("contatos")
-    fun insereContato(@Body contato:Contato): Call<Contato>
+        @DELETE("contatos/{id}")
+        fun excluiContato(@Path("id") id:String): Call<Void>
 
-    @PUT("contatos/{id}")
-    fun editaContato(@Body contato:Contato, @Path("id") id:String): Call<Contato>
+        @Multipart
+        @POST("upload")
+        fun uploadImagem(@Part imagem: MultipartBody.Part):
+                Call<UploadResposta>
 
-    @DELETE("contatos/{id}")
-    fun excluiContato(@Path("id") id:String): Call<Void>
+        data class UploadResposta(val nomeArquivo: String)
 
-    @Multipart
-    @POST("upload")
-    fun uploadImagem(@Part imagem: MultipartBody.Part):
-            Call<UploadResposta>
-
-    data class UploadResposta(val nomeArquivo: String)
-
- */
+     */
 }
