a80867be1dd6f47039bbdce9202beeedebd2d9b8
offtavs <otaviobarbosa2004@gmail.com>
2025-06-13 04:38:28 +0000

    CategoriaAdapter listaContato

--- app/src/main/java/com/example/app_fateclanches/adapter/CategoriaAdapter.kt
+++ app/src/main/java/com/example/app_fateclanches/adapter/CategoriaAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.app_fateclanches.adapter
 
-class CategoriaAdapter {
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.app_fateclanches.databinding.CategoriaItemBinding
+import com.example.app_fateclanches.models.Categoria
+
+class CategoriaAdapter (private  val context: Context, private val listaContato: MutableList<Categoria>):
+    RecyclerView.Adapter<CategoriaAdapter.CategoriaViewHolder>() {
+
+    inner class CategoriaViewHolder(binding: CategoriaItemBinding): RecyclerView.ViewHolder(binding.root){
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriaViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CategoriaViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
+
+
